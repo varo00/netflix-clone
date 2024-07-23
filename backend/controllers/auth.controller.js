eaf4e67fc6838763e995bda157d02cfc7e6c8dd1
Alvaro Gomez Garcia <44815166+varo00@users.noreply.github.com>
2024-07-23 21:02:10 +0200

    logout done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -66,5 +66,11 @@ export async function login(req, res) {
 }
 
 export async function logout(req, res) {
-    res.send("logout is ready");
+    try {
+        res.clearCookie("jwt-token");
+        res.status(200).json({ success: true, message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error logout controller: ", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
 }
