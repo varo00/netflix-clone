156bf3e5159fa5fc5dddd0fc374768b849b9bd0d
Alvaro Gomez Garcia <44815166+varo00@users.noreply.github.com>
2024-07-23 20:57:20 +0200

    init commit and signup done

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,70 @@
+import { User } from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../utils/generateToken.js';
+
+export async function signup(req, res) {
+    try {
+        const { email, password, username } = req.body;
+
+        // posibles errores
+        if (!email || !password || !username) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ success: false, message: "Invalid email" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ success: false, message: "Password must be at least 6 characters" });
+        }
+
+        const existingUserByEmail = await User.findOne({ email: email });
+        if (existingUserByEmail) {
+            return res.status(400).json({ success: false, message: "Email already exists" });
+        }
+
+        const existingUserByUsername = await User.findOne({ username: username });
+        if (existingUserByUsername) {
+            return res.status(400).json({ success: false, message: "Username already exists" });
+        }
+
+        // si todo va bien
+        const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"]; // tendrá sentido cuando haga el frontend
+        const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)]
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPwd = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({
+            email,
+            password: hashedPwd,
+            username,
+            image
+        });
+
+        generateTokenAndSetCookie(newUser._id, res);
+        await newUser.save();
+
+        return res.status(201).json({
+            success: true,
+            user: {
+                ...newUser._doc,
+                password: ""
+            }
+        });
+
+    } catch (error) {
+        console.log("Error signup controller: ", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
+export async function login(req, res) {
+    res.send("login is ready");
+}
+
+export async function logout(req, res) {
+    res.send("logout is ready");
+}
